d1bf43c4ed4fed5ebd27905188ce64695a1329dd
lgonzalezru <65631466+lgonzalezru@users.noreply.github.com>
2020-08-30 21:18:52 -0500

    NV

---
+++ send.php
@@ -0,0 +1,84 @@
+<?php
+
+/***librerias phpmailer**/
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+/* Exception class. */
+require 'PHPMailer/src/Exception.php';
+
+/* The main PHPMailer class. */
+require 'PHPMailer/src/PHPMailer.php';
+
+/* SMTP class, needed if you want to use SMTP. */
+require 'PHPMailer/src/SMTP.php';
+/**********/
+
+$imagen = $_POST["imagen"];
+$fisica2_regGP = $_POST["fisica2_regGP"];
+$fisica2_regCA = $_POST["fisica2_regCA"];
+$fisica2_regND = $_POST["fisica2_regND"];
+$fisica2_regCD = $_POST["fisica2_regCD"];
+$fisica2_regNA = $_POST["fisica2_regNA"];
+/*
+echo $imagen;
+echo "<br>";
+echo $fisica2_regGP;
+echo "<br>";
+echo $fisica2_regCA;
+echo "<br>";
+echo $fisica2_regND;
+echo "<br>";
+echo $fisica2_regCD;
+echo "<br>";
+echo $fisica2_regNA;
+echo "<br>";
+//die("fin");
+**/
+$imagen = preg_replace('#^data:image/[^;]+;base64,#', '', $imagen); 
+$mensaje = '<b>Fisica 2</b><br><b>Nombre del estudiante: </b>'.$fisica2_regNA.'<br><b>Grupo: </b> '.$fisica2_regGP.'<br><b>Docente: </b>'.$fisica2_regND;
+
+$para = $fisica2_regCD;
+$para2 = $fisica2_regCA;
+$asunto = 'Fisica 2: Actividad';				
+//Create a new PHPMailer instance
+$mail = new PHPMailer();
+$mail->IsSMTP();
+//Agregar la imagen
+$decode = base64_decode($imagen);
+$mail->addStringAttachment($decode, "Actividad.png", "base64", "image/png");
+$mensaje .= '<br><img src="https://majesticeducacion.com.mx/nuevo/wp-content/uploads/2018/08/logo-header-majesticeducacion.png">';
+ 
+//Configuracion servidor mail
+
+$mail->From = "ebook@majesticeducationdigital.com"; //remitente
+$mail->FromName = "Majestic Education";//nombre remitente
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'ssl'; //seguridad
+$mail->Host = "mail.majesticeducationdigital.com"; // servidor smtp
+$mail->Port = 465; //puerto
+$mail->Username ='ebook@majesticeducationdigital.com'; //nombre usuario
+$mail->Password = '[;$&0?H_zuq#'; //contraseña
+
+ 
+//Agregar destinatario
+$mail->AddAddress($para);
+$mail->AddAddress($para2);
+$mail->Subject = $asunto;
+$mail->IsHTML(true);
+$mail->Body = $mensaje;
+
+
+ 
+//Avisar si fue enviado o no y dirigir al index
+if ($mail->Send()) {
+    echo'<script type="text/javascript">
+           alert("Enviado correctamente");
+		   window.history.back();
+        </script>';
+} else {
+    echo'<script type="text/javascript">
+           alert("No enviado, intenta de nuevo");
+        </script>';
+}
+?>
